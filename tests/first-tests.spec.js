6b929a137a8d780ce1b7f45ff24a79b393004d98
baris_aksay <barish.aksay@gmail.com>
2024-08-19 15:54:38 +0200

    added a describe block

--- tests/first-tests.spec.js
+++ tests/first-tests.spec.js
@@ -1,22 +1,25 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { describe } = require('node:test');
 
-test("navigate to login page",async({page})=>{
-  await page.goto("https://www.saucedemo.com/v1/");
-  await expect(page).toHaveURL("https://www.saucedemo.com/v1/")
+describe("learning playwright",()=>{
+
+  test('test', async ({ page }) => {
+    const usernameField = page.locator('[data-test="username"]')
+    const passwordField = page.locator('[data-test="password"]')
+    const loginButton = page.locator('input#login-button')
+    const baseUrl = 'https://www.saucedemo.com/v1/'
+    const inventoryPageURL= 'https://www.saucedemo.com/v1/inventory.html'
+  
+    await page.goto(baseUrl);
+    await usernameField.click();
+    await usernameField.fill('standard_user');
+    await passwordField.click();
+    await passwordField.fill('secret_sauce');
+    await loginButton.click();
+    await expect(page).toHaveURL(inventoryPageURL)
+  });
+  
+
 })
 
-test('test', async ({ page }) => {
-  const usernameField = page.locator('[data-test="username"]')
-  const passwordField = page.locator('[data-test="password"]')
-  const loginButton = page.locator('input#login-button')
-  const baseUrl = 'https://www.saucedemo.com/v1/'
-  const inventoryPageURL= 'https://www.saucedemo.com/v1/inventory.html'
-  await page.goto(baseUrl);
-  await usernameField.click();
-  await usernameField.fill('standard_user');
-  await passwordField.click();
-  await passwordField.fill('secret_sauce');
-  await loginButton.click();
-  await expect(page).toHaveURL(inventoryPageURL)
-});
