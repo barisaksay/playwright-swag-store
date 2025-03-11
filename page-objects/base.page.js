4353f76fd9e9f17a8ed7795895935236a8394806
baris_aksay <barish.aksay@gmail.com>
2025-03-11 10:42:57 +0100

    basePage class added with element interaction methods

---
+++ page-objects/base.page.js
@@ -0,0 +1,24 @@
+class BasePage{
+    constructor(page){
+        this.page=page;
+        this.baseUrl="https://www.saucedemo.com/";
+    }
+
+    //navigation method
+    async navigate(path=''){
+        await this.page.goto(`${this.baseUrl}${path}`);
+    }
+
+    //element interaction
+    async clickElement(selector){
+        await this.page.locator(selector).click()
+    }
+
+    async fillInputField(selector,text){
+        await this.page.locator(selector). fill(text)
+    }
+
+    async getText(selector){
+        return await this.page.locator(selector).textContent()
+    }
+}
